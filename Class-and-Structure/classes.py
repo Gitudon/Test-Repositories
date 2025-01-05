b36a4f14cf6200518fbda4b40c4673b04a605fb7
Gitudon <oniku.udon.029@gmail.com>
2025-01-06 08:06:40 +0900

    作業領域の追加

---
+++ Class-and-Structure/classes.py
@@ -0,0 +1,14 @@
+class Person:
+    def __init__(self):
+        self.name = ""
+        self.age = 0
+    def profile(self):
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+
+
+def main():
+    pass
+
+if __name__=="__main__":
+    main()
