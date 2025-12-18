a41078283ea930f43c532a83d27ac465c3f805b6
Gitudon <oniku.udon.029@gmail.com>
2025-12-18 11:36:47 +0900

    Pythonのマクロ

--- Python-Macro/python_macro.py
+++ Python-Macro/python_macro.py
@@ -1,2 +1,27 @@
 class Macro:
-    pass
+
+    # 整数Nと、N要素のリストAを入力する
+    def input_list():
+        N = int(input())
+        A = [0] * N
+        for i in range(N):
+            A[i] = int(input())
+        return N, A
+
+    # 整数H, Wと、H行W列の行列を入力する
+    def input_matrix():
+        H, W = map(int, input().split())
+        matrix = [0] * H
+        for i in range(H):
+            matrix[i] = list(map(int, input().split()))
+        return H, W, matrix
+
+
+if __name__ == "__main__":
+    N, A = Macro.input_list()
+    print(N)
+    print(A)
+    H, W, matrix = Macro.input_matrix()
+    print(H)
+    print(W)
+    print(matrix)
